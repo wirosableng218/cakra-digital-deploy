43d9e32389fb8b6a104fedb0c734a2971ba8a159
wirosableng218 <yudhasaputro82@gmail.com>
2026-02-11 06:25:46 +0700

    Remove Payment link from client page navigation (desktop + mobile)

--- client/src/client.js
+++ client/src/client.js
@@ -234,9 +234,6 @@ function Client() {
             <li><Link to="/tentang">Tentang Kami</Link>
             </li>
             <li>
-              <Link to="/payment">Payment</Link>
-            </li>
-            <li>
               <Link to="/client">Client</Link>
             </li>
             <li>
@@ -287,9 +284,6 @@ function Client() {
             </li>
             <li><Link to="/tentang" onClick={() => setMobileOpen(false)}>Tentang Kami
             </Link>
-            </li>
-            <li>
-              <Link to="/payment" onClick={() => setMobileOpen(false)}>Payment</Link>
             </li>
             <li>
               <Link to="/client" onClick={() => setMobileOpen(false)}>Client
